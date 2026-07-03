262c0a217b5da2b5afcef451ac032a9ef4acbdfc
Martina Halldin <82marhal@gafe.molndal.se>
2026-07-03 14:38:42 +0200

    First design elements

---
+++ Migrations/20260703123341_AddImageUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio_Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_Slug",
+                table: "Projects",
+                column: "Slug",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_Slug",
+                table: "Projects");
+        }
+    }
+}
